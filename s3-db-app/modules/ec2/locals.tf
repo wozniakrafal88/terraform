2b4f4598bab00e965183534773de5ea0ede11e37
wozniakrafal88 <72942683+wozniakrafal88@users.noreply.github.com>
2022-10-20 10:25:53 +0200

    Delete s3-db-app directory

--- s3-db-app/modules/ec2/locals.tf
+++
@@ -1,7 +0,0 @@
-locals{
-s3_env_vars = ["BUCKET_NAME=${var.bucket_name}", "DB_API_URL=http://${aws_lb.load_balancer.dns_name}/db/API_number_of_books/"]
-db_env_vars =  ["RDSHOST=${var.rds_host}", "DB_DBNAME=${var.db_name}", "DB_PORT=${var.db_port}", "DB_USERNAME=${var.db_username}", "AWS_REGION=eu-west-1"]
-
-db_user_data = {yum_programs = var.db_yum_programs, pip_programs = var.db_pip_programs, git_url = var.db_git_url, env_variables = local.db_env_vars, app_name = var.db_app_name}
-s3_user_data = {yum_programs = var.s3_yum_programs, pip_programs = var.s3_pip_programs, git_url = var.s3_git_url, env_variables = local.s3_env_vars, app_name = var.s3_app_name}
-}
--- s3-db-app/modules/iam/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_iam_profile" {
-  value = aws_iam_instance_profile.s3_profile.name
-}
-
-output "rds_iam_profile" {
-  value = aws_iam_instance_profile.rds_profile.name
-}
