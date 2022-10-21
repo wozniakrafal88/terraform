28b0db27fad90c19cf57a0fe5ff41d83805bee75
wozniakrafal88 <72942683+wozniakrafal88@users.noreply.github.com>
2022-10-21 15:40:49 +0200

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -1,12 +1,3 @@
-data "terraform_remote_state" "infra_state"{
-  backend = "s3"
-    config = {
-      bucket = "rwozniak-tfstate"
-      key = "tfstate/terraform_infra.tfstate"
-      region  = "eu-west-1"
-  }
-}
-
 module "db_app" {
   source = "../modules/app"
 
