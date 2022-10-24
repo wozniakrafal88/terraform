47315e1c761d080d03a13c3efd609503804686b9
wozniakrafal88 <72942683+wozniakrafal88@users.noreply.github.com>
2022-10-24 14:17:55 +0200

    Delete app directory

--- app/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "infra_state"{
-  backend = "s3"
-    config = {
-      bucket = "rwozniak-tfstate"
-      key = "tfstate/terraform_infra.tfstate"
-      region  = "eu-west-1"
-  }
-}
--- app/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  env_name = "${terraform.workspace}"
-}
